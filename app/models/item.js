af96fc015170c2e36e59c046074d8acb8674da80
Carl Egbert and Colin W Kingen <colin@>
2016-08-24 14:58:28 -0700

    refactor shopping cart to use properties

--- app/models/item.js
+++ app/models/item.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   name: DS.attr(),
   cost: DS.attr(),
   category: DS.attr(),
+  quantity: DS.attr()
 });
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -3,13 +3,32 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   cartItems: [],
   cartTotal: 0,
+  itemCount: 0,
   add(item) {
-    this.get('cartItems').pushObject(item);
+    if (!this.get('cartItems').includes(item)) {
+      item.set('quantity', 1);
+      this.get('cartItems').pushObject(item);
+    } else {
+      var index = this.get('cartItems').indexOf(item);
+      var itemInArray = this.get('cartItems')[index];
+      itemInArray.set('quantity', itemInArray.get('quantity')+1);
+    }
+
+    var newCount = this.get('itemCount')+1;
+    this.set('itemCount', newCount);
     var newTotal = item.get('cost') + this.get('cartTotal');
     this.set('cartTotal', newTotal);
   },
   remove(item) {
-    this.get('cartItems').removeObject(item);
+    var index = this.get('cartItems').indexOf(item);
+    var itemInArray = this.get('cartItems')[index];
+    if (itemInArray.get('quantity') === 1) {
+      this.get('cartItems').removeObject(item);
+    } else {
+      itemInArray.set('quantity', itemInArray.get('quantity')-1);
+    }
+    var newCount = this.get('itemCount')-1;
+    this.set('itemCount', newCount);
     var newTotal = this.get('cartTotal') - item.get('cost');
     this.set('cartTotal', newTotal);
   }

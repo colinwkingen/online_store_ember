47ed3e64d455a7d45ea998b331ead7b44d41bd88
Carl Egbert and Colin W Kingen <carl@>
2016-08-24 11:44:10 -0700

    finish remove functionality and add item amount to service

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -6,5 +6,16 @@ export default Ember.Service.extend({
   add(item) {
     this.get('cartItems').pushObject(item);
     console.log(this.get('cartItems'));
+  },
+  remove(item) {
+    this.get('cartItems').removeObject(item);
+  },
+  totalPrice() {
+    var totalCost = 0;
+    this.get('cartItems').forEach(function(item){
+      totalCost += item.cost;
+    });
+    return totalCost;
   }
+
 });
